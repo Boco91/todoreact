ed4975b4c6ac08b3f372ef3c67ccbd488f348bfb
Borislavko Djumic <49570923+Boco91@users.noreply.github.com>
2020-01-19 17:16:22 +0100

    Local storage and style added.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,56 @@ export default class App extends Component {
     };
   }
 
+  //incorporating local storage
+  componentDidMount() {
+    this.hydrateStateWithLocalStorage();
+
+    // add event listener to save state to localStorage
+    // when user leaves/refreshes the page
+    window.addEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+
+    // saves if component has a chance to unmount
+    this.saveStateToLocalStorage();
+  }
+
+  hydrateStateWithLocalStorage() {
+    // for all items in state
+    for (let key in this.state) {
+      // if the key exists in localStorage
+      if (localStorage.hasOwnProperty(key)) {
+        // get the key's value from localStorage
+        let value = localStorage.getItem(key);
+
+        // parse the localStorage string and setState
+        try {
+          value = JSON.parse(value);
+          this.setState({ [key]: value });
+        } catch (e) {
+          // handle empty string
+          this.setState({ [key]: value });
+        }
+      }
+    }
+  }
+
+  saveStateToLocalStorage() {
+    // for every item in React state
+    for (let key in this.state) {
+      // save to localStorage
+      localStorage.setItem(key, JSON.stringify(this.state[key]));
+    }
+  }
+
   updateInput = (key, value) => {
     console.log("update");
     //update react state
@@ -53,28 +103,48 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div>
-          Task to do
-          <br />
-          <input
-            type="text"
-            placeholder="Enter new item..."
-            value={this.state.newItem}
-            onChange={e => this.updateInput("newItem", e.target.value)}
-          />
-          <button onClick={() => this.addItem()}>Add</button>
-          <br />
-          <ul>
-            {this.state.list.map(item => {
-              return (
-                <li key={item.id}>
-                  {item.value}{" "}
-                  <button onClick={() => this.deleteItem(item.id)}>X</button>
-                </li>
-              );
-            })}
-          </ul>
+      <div>
+        <h1 className="app-title">Task to do</h1>
+        <div className="container">
+          <div
+            style={{
+              padding: 30,
+              textAlign: "center",
+              maxWidth: 500,
+              margin: "auto"
+            }}
+          >
+            <br />
+            <input
+              className="input"
+              type="text"
+              placeholder="Enter new item..."
+              value={this.state.newItem}
+              onChange={e => this.updateInput("newItem", e.target.value)}
+            />
+            <button
+              className="add-btn btn-floating"
+              onClick={() => this.addItem()}
+            >
+              Add new item
+            </button>
+            <br />
+            <ul>
+              {this.state.list.map(item => {
+                return (
+                  <li key={item.id}>
+                    {item.value}{" "}
+                    <button
+                      className="delete-btn"
+                      onClick={() => this.deleteItem(item.id)}
+                    >
+                      Delete item
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     );
